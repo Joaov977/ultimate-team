51599bd9246b0446f734c1bb51ffd53e5a8f3549
Joao <joaovitorveras1@gmail.com>
2026-06-05 14:21:07 -0300

    Implementando limite de 11 jogadores e escolha de capitão

--- UltimateTeam.java
+++ UltimateTeam.java
@@ -9,19 +9,35 @@ public class UltimateTeam {
 
         ArrayList<Jogador> time = new ArrayList<>();
 
+        Jogador capitao = null;
+
         while (true) {
 
             System.out.println("\n=== ULTIMATE TEAM ===");
+            System.out.println("Jogadores: " + time.size() + "/11");
+            if (capitao != null) {
+
+                System.out.println("Capitão: " + capitao.nome);
+
+                }
             System.out.println("1 - Adicionar jogador");
             System.out.println("2 - Ver time");
             System.out.println("3 - Calcular overall");
             System.out.println("4 - Melhor jogador");
+            System.out.println("5 - Definir capitão");
             System.out.println("0 - Sair");
 
             int opcao = sc.nextInt();
             sc.nextLine();
 
             if (opcao == 1) {
+
+                if (time.size() >= 11) {
+
+                    System.out.println("Elenco completo! Máximo de 11 jogadores.");
+
+                    continue;
+                }
 
                 System.out.print("Nome: ");
                 String nome = sc.nextLine();
@@ -45,12 +61,22 @@ public class UltimateTeam {
 
                 System.out.println("\n=== SEU TIME ===");
 
-                for (Jogador j : time) {
-                    System.out.println(
-                        j.nome + " | " +
-                        j.overall + " | " +
-                        j.posicao
-                    );
+                if (time.size() == 0) {
+
+                    System.out.println("Time vazio!");
+
+                } else {
+
+                    for (Jogador j : time) {
+
+                        System.out.println(
+                            j.nome + " | " +
+                            j.overall + " | " +
+                            j.posicao
+                        );
+
+                    }
+
                 }
 
             }
@@ -72,7 +98,9 @@ public class UltimateTeam {
                     );
 
                 } else {
+
                     System.out.println("Time vazio!");
+
                 }
 
             }
@@ -86,33 +114,91 @@ public class UltimateTeam {
                     for (Jogador j : time) {
 
                         if (j.overall > melhor.overall) {
+
                             melhor = j;
+
                         }
 
                     }
+                    
 
-        System.out.println("\n⭐ MELHOR JOGADOR");
+                    System.out.println("\n⭐ MELHOR JOGADOR");
 
-        System.out.println(
-            melhor.nome + " | " +
-            melhor.overall + " | " +
-            melhor.posicao
-        );
+                    System.out.println(
+                        melhor.nome + " | " +
+                        melhor.overall + " | " +
+                        melhor.posicao
+                    );
 
-    } else {
+                } else {
+
+                    System.out.println("Time vazio!");
+
+                }
+
+            }
+
+            else if (opcao == 5) {
+
+    if (time.size() == 0) {
 
         System.out.println("Time vazio!");
 
-    }
+    } else {
 
-}
+        System.out.println("\n=== ESCOLHER CAPITÃO ===");
+
+        for (int i = 0; i < time.size(); i++) {
+
+            Jogador j = time.get(i);
+
+            System.out.println(
+                (i + 1) + " - " +
+                j.nome + " | " +
+                j.overall + " | " +
+                j.posicao
+            );
+
+        }
+
+        System.out.print("Escolha o número do jogador: ");
+        int escolha = sc.nextInt();
+        sc.nextLine();
+
+        if (escolha >= 1 && escolha <= time.size()) {
+
+            capitao = time.get(escolha - 1);
+
+            System.out.println(
+                capitao.nome + " agora é o capitão do time!"
+            );
+
+        } else {
+
+            System.out.println("Jogador inválido!");
+
+                }
+
+            }
+
+        }
 
             else if (opcao == 0) {
+
                 break;
+
+            }
+
+            else {
+
+                System.out.println("Opção inválida!");
+
             }
 
         }
 
         sc.close();
+
     }
+
 }
